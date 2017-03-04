d59985e08f3c87677c1959b534823010d1fffa8f
Hugh Wang <hghwng@gmail.com>
2017-03-04 22:59:05 +0800

    wp: refactor; pass screen index to filter

--- wp.lua
+++ wp.lua
@@ -20,14 +20,14 @@ local function load_database(root)
 end
 
 -- Filter images loaded from database
-local function filter_image(images, filters)
+local function filter_image(s, images, filters)
   local output = {}
   local output_idx = 0
   for image_idx, image in pairs(images) do
     -- Only allow images that pass the check
     local pass = true
     for _, filter in pairs(filters) do
-      if not filter(image_idx, image) then goto fail end
+      if not filter(s, image_idx, image) then goto fail end
     end
     output[output_idx] = image
     output_idx = output_idx + 1
@@ -36,10 +36,31 @@ local function filter_image(images, filters)
   return output
 end
 
+-- Filter the images, and set the image as wallpaper
+local function do_set(s, images, filter, setter, root)
+  local filtered_images = filter_image(s, images, filters)
+  if #filtered_images == 0 then return end
+  local the_image = filtered_images[math.random(#filtered_images)]
+  setter(s, the_image, root .. "/" .. the_image.path)
+end
+
+-- Prepare the database, filters and setter for given settings
+-- settings:
+--   root: (required) root directory to the wallpapers
+--   filters: a series of filter functions
+--   setter: a function that actually set the wallpaper
+local function prepare(settings)
+  local images = load_database(settings.root)
+  local filters = settings.filters or { wp.filter_by_ratio(1) }
+  local setter = settings.setter or wp.set_maximized
+  return images, filters, setter
+end
+
 -- Ignore images whose ratio significantly differ from the screen ratio
-function wp.filter_by_ratio(max_diff, geometry)
+function wp.filter_by_ratio(max_diff)
   local naughty = require("naughty")
-  return function(image_idx, image)
+  return function(s, image_idx, image)
+    local geometry = screen[s].geometry
     return math.abs(image.width / image.height - geometry.width / geometry.height) < max_diff
   end
 end
@@ -51,20 +72,18 @@ function wp.set_maximized(s, image, image_path)
   gears.wallpaper.maximized(image_path, s)
 end
 
--- Set the image
--- settings:
---   root: (required) root directory to the wallpapers
---   filters: a series of filter functions
---   setter: a function that actually set the wallpaper
-function wp.set(s, settings)
-  local images = load_database(settings.root)
-  local filters = settings.filters or { wp.filter_by_ratio(0.5, screen[s].geometry) }
-  local setter = settings.setter or wp.set_maximized
+-- Set wallpaper for one given screen
+function wp.set_one(s, settings)
+  images, filters, setter = prepare(settings)
+  do_set(s, images, filters, setter, settings.root)
+end
 
-  local filtered_images = filter_image(images, filters)
-  if #filtered_images == 0 then return end
-  local the_image = filtered_images[math.random(#filtered_images)]
-  setter(s, the_image, settings.root .. "/" .. the_image.path)
+-- Set wallpaper for all screens
+function wp.set_all(settings)
+  images, filters, setter = prepare(settings)
+  for s = 1, screen.count() do
+    do_set(s, images, filters, setter, settings.root)
+  end
 end
 
 math.randomseed(os.time());
